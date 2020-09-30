41b90b3ae5cdad900537d5d40ca85d8f401c84ee
Matthias Wolf <matthias.wolf@epfl.ch>
2020-09-30 13:02:35 +0200

    deploy: d-bug

--- lib/spack/spack/installer.py
+++ lib/spack/spack/installer.py
@@ -985,7 +985,7 @@ class PackageInstaller(object):
                     self._add_bootstrap_compilers(dep_pkg, request)
 
                 dep_id = package_id(dep_pkg)
-                tty.debug('MINE >>>>>> {0}'.format(self.dep_id))
+                tty.debug('MINE >>>>>> {0}'.format(dep_id))
                 if dep_id not in self.build_tasks:
                     self._push_task(dep_pkg, request, False, 0, 0,
                                     STATUS_ADDED)
@@ -1004,7 +1004,7 @@ class PackageInstaller(object):
                 # associated with another process in this parallel build
                 # of the spec.
                 spack.store.db.clear_failure(dep, force=False)
-                tty.debug('MINE <<<<<< {0}'.format(self.dep_id))
+                tty.debug('MINE <<<<<< {0}'.format(dep_id))
 
             # Push any missing compilers (if requested) as part of the
             # package dependencies.
